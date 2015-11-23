0dda0191101a1de88779563e2cfbbb3056d2eb22
sebastianusbara <sebastianus.bara@gmail.com>
2015-11-23 12:30:17 +0700

    Form Validation

--- day6/assets/js/main.min.js
+++ day6/assets/js/main.min.js
@@ -19,6 +19,7 @@
             Site.fastClick();
             Site.enableActiveStateMobile();
             Site.WPViewportFix();
+            Site.formValidation();
 
             window.Site = Site;
         },
@@ -41,22 +42,118 @@
         WPViewportFix: function () {
             if ( navigator.userAgent.match(/IEMobile\/10\.0/) ) {
                 var style   = document.createElement("style"),
-                    fix     = document.createTextNode("@-ms-viewport{width:auto!important}");
+                fix     = document.createTextNode("@-ms-viewport{width:auto!important}");
 
                 style.appendChild(fix);
                 document.getElementsByTagName('head')[0].appendChild(style);
             }
-        }
+        },
 
+        formValidation: function () {
+            var $input      = $( "#fullname,#email,#password,#hobby,#address" );
+            var $fullname   = $( "#fullname" );
+            var $email      = $( "#email" );
+            var $password   = $( "#password" );
+            var $hobby      = $( "#hobby" );
+            var $address    = $( "#address" );
+            var $emailRegex = '^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$'; 
+            var $submit     = $( "#submit" );
+            $submit.on( "click", 
+                function (e) {
+                $("form .err-msg").remove();
+                    if ($fullname.val().length > 0) {
+                        $fullname.addClass("form-input--success");
+                        $fullname.removeClass("form-input--error");
+                        $("#fullname-row .err-msg").remove();
+                        e.preventDefault(e);
+                    }
+
+                    else if ($fullname.val().length == 0) {
+                        $fullname.addClass("form-input--error");
+                        $fullname.removeClass("form-input--success");
+                        $fullname.after('<div class="err-msg">This Field Cannot Left Blank</div>');
+                        e.preventDefault(e);
+                    }
+
+                    else if ($fullname.is(":focus")) {
+                        $("#fullname-row .err-msg").remove();
+                    }
+
+                    if ($email.val().length > 0) {
+                        $email.addClass("form-input--success");
+                        $email.removeClass("form-input--error");
+                        $("#email-row .err-msg").remove();
+                        e.preventDefault(e);
+                    }
+
+                    else if ($email.val().length == 0) {
+                        $email.addClass("form-input--error");
+                        $email.removeClass("form-input--success");
+                        $email.after('<div class="err-msg">This Field Cannot Left Blank</div>');
+                        e.preventDefault(e);
+                    }
+
+                    // else if ($emailRegex == false) {
+                    //     $email.addClass("form-input--error");
+                    //     $email.removeClass("form-input--success");
+                    //     $email.after('<div class="err-msg">Your email input is invalid, please insert valid e-mail address</div>');
+                    //     e.preventDefault(e);
+                    // }
+
+                    if ($password.val().length > 0) {
+                        $password.addClass("form-input--success");
+                        $password.removeClass("form-input--error");
+                        $("#password-row .err-msg").remove();
+                        e.preventDefault(e);
+                    }
+
+                    else if ($password.val().length == 0) {
+                        $password.addClass("form-input--error");
+                        $password.removeClass("form-input--success");
+                        $password.after('<div class="err-msg">This Field Cannot Left Blank</div>');
+                        e.preventDefault(e);
+                    }
+
+                    if ($hobby.val().length > 0) {
+                        $hobby.addClass("form-input--success");
+                        $hobby.removeClass("form-input--error");
+                        $("#hobby-row .err-msg").remove();
+                        e.preventDefault(e);
+                    }
+
+                    else if ($hobby.val().length == 0) {
+                        $hobby.addClass("form-input--error");
+                        $hobby.removeClass("form-input--success");
+                        $hobby.after('<div class="err-msg">This Field Cannot Left Blank</div>');
+                        e.preventDefault(e);
+                    }
+
+                    if ($address.val().length > 0) {
+                        $address.addClass("form-input--success");
+                        $address.removeClass("form-input--error");
+                        $("#address-row .err-msg").remove();
+                        e.preventDefault(e);
+                    }
+
+                    else if ($address.val().length == 0) {
+                        $address.addClass("form-input--error");
+                        $address.removeClass("form-input--success");
+                        $address.after('<div class="err-msg">This Field Cannot Left Blank</div>');
+                        e.preventDefault(e);
+                    }
+
+                }
+            )
+        }
     };
 
     var checkJquery = function () {
         Modernizr.load([
-            {
-                test    : window.jQuery,
-                nope    : assets._jquery_local,
-                complete: Site.init
-            }
+        {
+            test    : window.jQuery,
+            nope    : assets._jquery_local,
+            complete: Site.init
+        }
         ]);
     };
 
