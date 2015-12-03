9a713226ee47fbaeb1a07751799c9c6da05e1c4b
sebastianusbara <sebastianus.bara@gmail.com>
2015-12-03 14:28:37 +0700

    Initialize Edulab Repo

--- day8/dev/js/main.js
+++ day8/dev/js/main.js
@@ -10,35 +10,38 @@
     var assets = {
         _jquery_cdn     : 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js',
         _jquery_local   : path.js + 'jquery.min.js',
-        _jquery_migrate : path.js + 'jquery-migrate.min.js',
+        _jquery_migrate : 'http://code.jquery.com/jquery-migrate-1.2.1.min.js',
         _fastclick      : path.js + 'fastclick.min.js',
-        _slickslider    : path.js + 'slick.min.js'
+        // _slickslider    : path.js + 'slick.min.js'
     };
 
     var Site = {
 
         init: function () {
-            Site.jqueryMigrate();
+            // Site.jqueryMigrate();
             Site.fastClick();
-            Site.slickSlider();
+            // Site.slickSlider();
             Site.enableActiveStateMobile();
             Site.WPViewportFix();
             Site.accordionSlide();
             Site.toggleButton();
             Site.toggleSubmenu();
             // Site.sliderAutoplay();
+            Site.toggleSidebar();
+            Site.minatTabs();
+            Site.buttonForm();
 
             window.Site = Site;
         },
 
-        jqueryMigrate: function () {
-            Modernizr.load({
-                load    : assets._jquery_migrate,
-                complete: function () {
-                    JqueryMigrate.attach(document.body);
-                }
-            });
-        },
+        // jqueryMigrate: function () {
+        //     Modernizr.load({
+        //         load    : assets._jquery_migrate,
+        //         complete: function () {
+        //             jquerymigrate.attach(document.body);
+        //         }
+        //     });
+        // },
 
         fastClick: function () {
             Modernizr.load({
@@ -49,14 +52,14 @@
             });
         },
 
-        slickSlider: function () {
-            Modernizr.load({
-                load    : assets._slickslider,
-                complete: function () {
-                    slickSlider.attach(document.body);
-                }
-            });
-        },
+        // slickSlider: function () {
+        //     Modernizr.load({
+        //         load    : assets._slickslider,
+        //         complete: function () {
+        //             slickSlider.attach(document.body);
+        //         }
+        //     });
+        // },
 
         enableActiveStateMobile: function () {
             if ( document.addEventListener ) {
@@ -100,8 +103,48 @@
                 $(this).siblings('.nav__submenu').toggleClass('show2 hidden');
                 $(this).find('.fa-mob').toggleClass('fa-chevron-down fa-chevron-up');
             });
-        }
+        },
 
+        toggleSidebar: function () {
+            var $toggleButton  = $('.main__toggle');
+            
+            $toggleButton.on ( 'click', function() {
+                $(this).parents('.main-content').siblings('.nav').toggleClass('show hidden');
+                $(this).parents('.main-content').toggleClass('full compact');
+            });
+        },
+
+        minatTabs: function () {
+            var $toggleTalent = $('.talent-tab');
+            var $toggleFinger = $('.fingerprint-tab');
+            
+            $toggleTalent.on ( 'click', function() {
+                $(this).parents('.post__tabs').siblings('.fingerprint').removeClass('show');
+                $(this).parents('.post__tabs').siblings('.fingerprint').addClass('hidden');
+                $(this).parents('.post__tabs').siblings('.talent').removeClass('hidden');
+                $(this).parents('.post__tabs').siblings('.talent').addClass('show');
+                $(this).parents('.post__tabs').find('.post__tabs__active').removeClass('post__tabs__active');
+                $(this).parent().addClass('post__tabs__active');
+            });
+
+            $toggleFinger.on ( 'click', function() {
+                $(this).parents('.post__tabs').siblings('.talent').removeClass('show');
+                $(this).parents('.post__tabs').siblings('.talent').addClass('hidden');
+                $(this).parents('.post__tabs').siblings('.fingerprint').removeClass('hidden');
+                $(this).parents('.post__tabs').siblings('.fingerprint').addClass('show');
+                $(this).parents('.post__tabs').find('.post__tabs__active').removeClass('post__tabs__active');
+                $(this).parent().addClass('post__tabs__active');
+            });
+        },
+
+        buttonForm: function () {
+            var $buttonForm     = $('.register .btn');
+            var $registerForm   = $('.register');
+
+                $registerForm.on ( 'click', '.btn', function() {
+                $(this).toggleClass('btn--active btn--deactive');
+            });
+        }
         // sliderAutoplay: function () {
         //     $('.autoplay').slick({
         //       slidesToShow: 2,
